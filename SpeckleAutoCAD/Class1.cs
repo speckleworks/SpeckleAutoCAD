e29d7e716325542877763d91045d6f506a306fff
Joost.Schlebaum <Joost.Schlebaum@arup.com>
2019-06-19 10:38:22 +0200

    setting up dependencies

--- SpeckleAutoCAD/Class1.cs
+++ SpeckleAutoCAD/Class1.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.Civil.ApplicationServices;
+using SpeckleUiBase;
 
 namespace SpeckleAutoCAD
 {
-    public class Class1
+    public class Class1 : IExtensionApplication
     {
+        public static SpeckleUiWindow SpeckleWindow;
+
+        #region IExtensionApplication Members
+
+
+
+        public void Initialize()
+        {
+ //           throw new System.Exception("The method or operation is not implemented.");
+        }
+
+        public void Terminate()
+        {
+ //           throw new System.Exception("The method or operation is not implemented.");
+        }
+
+        #endregion  
+
+        [CommandMethod("Speckle")]
+        public void Speckle()
+        {
+            // Gets the current document, and prints some basic information about it
+            // to the command editor window.
+
+            var bindings = new SpeckleUIBindings();
+
+            CivilDocument doc = Autodesk.Civil.ApplicationServices.CivilApplication.ActiveDocument;
+            ObjectIdCollection alignments = doc.GetAlignmentIds();
+            ObjectIdCollection sites = doc.GetSiteIds();
+            String docInfo = String.Format("\nHello Speckle!");
+            Application.DocumentManager.MdiActiveDocument.Editor.WriteMessage(docInfo);
+
+            SpeckleWindow = new SpeckleUiWindow(bindings);
+
+        }
     }
 }
